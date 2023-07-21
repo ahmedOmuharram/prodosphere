e70137175c7e408cf126c28176bddffe5aade1ce
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-07-21 22:18:18 +0300

    Undo previous issue fix

--- src/App.js
+++ src/App.js
@@ -139,8 +139,8 @@ function App() {
 
             <p style={{fontSize: "calc(30px + 1vmin)", marginTop: "0", paddingTop: "0"}}>
               {user === "" ? "Please enter your name" : 
-                (weatherState !== null && `${weatherState.weather[0].icon.charAt(2)}` === "n" ? `Good Evening, ${user}!` : 
-                (weatherState !== null && `${weatherState.weather[0].icon.charAt(2)}` === "d" ? `Good Morning, ${user}!` : `Hello, ${user}!`))}
+                (weatherState !== null && `${weatherState.weather[0].icon.charAt(2)}` === "n" ? `Good evening, ${user}!` : 
+                (weatherState !== null && `${weatherState.weather[0].icon.charAt(2)}` === "d" ? `Good morning, ${user}!` : `Hello, ${user}!`))}
             </p>
 
             <p style={{position: "absolute", top: "0px", right: "10px", textAlign: "right", fontSize: "40px", marginBottom: "-30px"}}>
