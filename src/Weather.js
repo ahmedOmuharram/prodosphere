4066660b252cfe6963a9ff6247f273cab2771bc3
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-07-21 21:53:17 +0300

    Remove console.logs

--- src/Weather.js
+++ src/Weather.js
@@ -10,13 +10,10 @@ function WeatherComponent( lat ) {
   const fetchWeatherData = async () => {
     try {
       const apiKey = process.env.REACT_APP_API_KEY;
-      console.log(apiKey)
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
 
       const response = await fetch(apiUrl);
-      console.log(response)
       const data = await response.json();
-      console.log(data)
 
       setWeatherData(data);
     } catch (error) {
@@ -41,7 +38,6 @@ function WeatherComponent( lat ) {
                     <img
                     src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
                     style={{ width: "60px", height: "60px", marginRight: "-10px", marginBottom: "5px", paddingRight: "0px" }}
-                    alt={weatherData.weather[0].main}
                     />
                     {weatherData.weather[0].main} | 🌡️ {Math.round(weatherData.main.temp)}°C
                 </div>
