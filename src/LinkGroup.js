cdaa6c2c8978961ce6d7cd54d927d262ae4dbfc3
youssefsaleh61 <youssefsaleh61@yahoo.com>
2023-07-22 14:14:00 +0300

    Overhaul bookmark tab

--- src/LinkGroup.js
+++ src/LinkGroup.js
@@ -5,9 +5,12 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 function LinkGroupComponent() {
   const [toolbars, setToolbars] = useState([[]]);
+  const [links, setLinks] = useState([]);
+  const [editorIndex, setEditorIndex] = useState(0);
   const maxButtonsPerToolbar = 10;
   const maxButtonNumber = 30;
   const [lastButtonNumber, setLastButtonNumber] = useState(0);
+
 
   const handleAddButton = () => {
     if (lastButtonNumber >= maxButtonNumber) {
@@ -15,6 +18,7 @@ function LinkGroupComponent() {
     }
 
     if (toolbars.length === 0 || toolbars[toolbars.length - 1].length >= maxButtonsPerToolbar) {
+      setLinks((links) => [...links, ""])
       setToolbars((prevToolbars) => [...prevToolbars, []]);
     }
 
@@ -36,6 +40,8 @@ function LinkGroupComponent() {
         <ButtonToolbar>
           <ButtonGroup className="me-2">
             {toolbar.map((buttonNumber) => (
+              <div>
+              <a href={links[buttonNumber]} target="_blank">
               <Button
                 className="btn btn-secondary"
                 style={{
@@ -50,6 +56,54 @@ function LinkGroupComponent() {
               >
                 {buttonNumber}
               </Button>
+              </a>
+              {editorIndex !== buttonNumber ? 
+              <Button
+              onClick={() => {
+                setEditorIndex(buttonNumber);
+              }}
+              className="btn btn-secondary"
+              style={{
+                border: "2px solid #555",
+                width: "5px",
+                height: "5px",
+                top: "18px",
+                position: "relative"
+              }}
+              key={buttonNumber}
+              
+            > </Button>:
+              <input
+                onChange={(event) => {
+                }}
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                  const newLinks = [...links];
+                  newLinks[buttonNumber] = event.currentTarget.value;
+                  setEditorIndex(0);
+                  setLinks(newLinks);
+                  }
+                }}
+                style={{
+                  fontSize: "15px",
+                  width: "90%",
+                  backgroundColor: "rgba(0,0,0,0)",
+                  color: "white",
+                  outline: "none",
+                  border: "none",
+                  borderBottom: "2px solid rgba(255, 255, 255, 1)",
+                }}
+                onBlur={(event) => {
+                  const newLinks = [...links];
+                  newLinks[buttonNumber] = event.currentTarget.value;
+                  setEditorIndex(0);
+                  setLinks(newLinks);
+                }}
+                autoFocus
+              />
+            }
+
+            </div>
             ))}
           </ButtonGroup>
         </ButtonToolbar>
