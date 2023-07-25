c0356521579a284cf566c7784c75c2f62cdf6ab7
youssefsaleh61 <youssefsaleh61@yahoo.com>
2023-07-25 13:17:01 +0300

    Add animation to changing menus

--- src/MenuItem.tsx
+++ src/MenuItem.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+import { useState, useContext } from "react";
+import { menuContext, clickContext } from "./App"
 import { motion } from "framer-motion";
 
 const variants = {
@@ -22,13 +23,20 @@ const colors = ["#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF"];
 
 export const MenuItem = ({ i }) => {
   const style = { border: `2px solid ${colors[i]}` };
+  const { menuState, setMenuState } = useContext(menuContext);
+  const { clickState, setClickState } = useContext(clickContext);
   return (
     <motion.li
       variants={variants}
       whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.98 }}
     >
-      <div className="icon-placeholder" style={style} />
+      <div className="icon-placeholder" style={style} onClick={() => {
+        if (menuState!==i) {
+          setMenuState(i);
+          setClickState(true);
+        }
+        }}></div>
     </motion.li>
   );
 };
