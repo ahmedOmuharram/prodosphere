95ccc111bffdc5db996b3051912b2dc70311903f
youssefsaleh61 <youssefsaleh61@yahoo.com>
2023-07-26 16:38:16 +0300

    Add translation component

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,7 +1,10 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 import { animateValue, motion } from "framer-motion";
 import { menuContext, clickContext } from "./App"
 import { MenuItem } from "./MenuItem.tsx";
+import axios from 'axios';
+
+
 
 const variants = {
   open: {
@@ -48,6 +51,42 @@ const menuClickedVariants = {
 export const Navigation = () => {
   const { menuState, setMenuState } = useContext(menuContext);
   const { clickState, setClickState } = useContext(clickContext);
+  const [ languagesList, setLanguagesList ] = useState([])
+  const [ translateState, setTranslateState ] = useState("");
+  const [ selectedLanguageFromKey, setLanguageFromKey ] = useState("")
+  const [ selectedLanguageKey, setLanguageKey ] = useState("")
+  const [resultText, setResultText] = useState("");
+  useEffect(() => {
+    axios.get(`https://libretranslate.de/languages`)
+    .then((response) => {
+      setLanguagesList(response.data)
+    })
+  }, [])
+  const getLanguageSource = () => {
+    axios.post(`https://libretranslate.de/detect`, {
+      q: translateState
+    })
+    .then((response) => {
+      setLanguageFromKey(response.data[0].language)
+    })
+  }
+  const languageFromKey =  (selectedLanguage) => {
+    setLanguageFromKey(selectedLanguage.target.value);
+  }
+  const languageKey =  (selectedLanguage) => {
+    setLanguageKey(selectedLanguage.target.value)
+  } 
+  const translateText = () => {
+    let data = {
+      q : translateState,
+      source: selectedLanguageFromKey,
+      target: selectedLanguageKey
+    }
+    axios.post(`https://libretranslate.de/translate`, data)
+    .then((response) => {
+      setResultText(response.data.translatedText)
+    })
+  } 
   return (
   <>
   <motion.ul variants={variants}>
@@ -73,13 +112,57 @@ export const Navigation = () => {
       height: "500px"
     }}
   >
-    {menuState === 0 && <p style={{color :"white"}}>0 GONNA</p>}
-    {menuState === 1 && <p style={{color :"white"}}>1 GIVE</p>}
-    {menuState === 2 && <p style={{color :"white"}}>2 YOU</p>}
-    {menuState === 3 && <p style={{color :"white"}}>3 UP</p>}
-    {menuState === 4 && <p style={{color :"white"}}>4 GONNA</p>}
-    {menuState === 5 && <p style={{color :"white"}}>5 SAY</p>}
-    {menuState === 6 && <p style={{color :"white"}}>6 GOODBYE</p>}
+    {menuState === 0 && <p style={{color :"white"}}>0</p>}
+    {menuState === 1 && <p style={{color :"white"}}>1</p>}
+    {menuState === 2 && <p style={{color :"white"}}>2</p>}
+    {menuState === 3 && <p style={{color :"white"}}>3</p>}
+    {menuState === 4 &&
+    <>
+    <select className="language-select" onChange={languageFromKey}>
+      <option value={""}>Please select from language...</option>
+      {languagesList.map((language) => {
+        return (
+          <option value={language.code}>
+            {language.name}
+          </option>
+        )
+      })}
+    </select>
+    <select className="language-select" onChange={languageKey}>
+      <option value={""}>Please Select to Language...</option>
+      {languagesList.map((language) => {
+        return (
+          <option value={language.code}>
+            {language.name}
+          </option>
+        )
+      })}
+    </select>
+    <input
+      type="text"
+      placeholder='Type Text to Translate..'
+      onChange={(e) => {
+        setTranslateState(e.target.value.replace("?", ""));
+      }}
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          if (selectedLanguageFromKey !== "" && selectedLanguageKey !== "" && translateState !== "") {
+            translateText();
+          }
+        }
+      }}
+    />
+    {(selectedLanguageFromKey !== "" && selectedLanguageKey !== "" && translateState !== "") && <button
+      color="orange"
+      size="large"
+      onClick={translateText}
+    >
+      Translate</button>}
+    <span style={{color: "white"}}>{resultText}</span>
+    </>
+    }
+    {menuState === 5 && <p style={{color :"white"}}>5</p>}
+    {menuState === 6 && <p style={{color :"white"}}>6</p>}
     {menuState === 1}
   </motion.div>
   </>
