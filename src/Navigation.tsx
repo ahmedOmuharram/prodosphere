2dffb47b96a8e3ee6ef5b714e54d8628d050f838
youssefsaleh61 <youssefsaleh61@yahoo.com>
2023-07-28 09:41:36 +0300

    Add timezone converter

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -8,15 +8,38 @@ import { menuContext, clickContext } from "./App"
 import { MenuItem } from "./MenuItem.tsx";
 import axios from 'axios';
 import YouTube from 'react-youtube';
+import Moment from 'react-moment';
+import moment from 'moment';
+import 'moment-timezone';
+
+const countryCodes = moment.tz.countries();
+const countryZones = [];
+countryCodes.forEach(countryCode => {
+  const names = moment.tz.zonesForCountry(countryCode, true);
+  names.forEach(name => {
+    let i = 0;
+    for (i = 0; i < countryZones.length; i++) {
+      if (name.name === countryZones[i].name) {
+        break;
+      }
+    }
+    if (i === countryZones.length) {
+      countryZones.push(name);
+    }
+  });
+});
+countryZones.sort(function (a, b) { return b.offset - a.offset });
+
+
 
 type MenuContextType = {
-  menuState: number; 
-  setMenuState: (newState: number) => void; 
+  menuState: number;
+  setMenuState: (newState: number) => void;
 };
 
 type ClickStateType = {
-  clickState: boolean; 
-  setClickState: (newState: boolean) => void; 
+  clickState: boolean;
+  setClickState: (newState: boolean) => void;
 };
 
 const variants = {
@@ -64,19 +87,23 @@ const menuClickedVariants = {
 export const Navigation = () => {
   const { menuState } = useContext<MenuContextType>(menuContext);
   const { clickState, setClickState } = useContext<ClickStateType>(clickContext);
-  const [ languagesList, setLanguagesList ] = useState([])
-  const [ translateState, setTranslateState ] = useState("");
-  const [ selectedLanguageFromKey, setLanguageFromKey ] = useState("")
-  const [ selectedLanguageKey, setLanguageKey ] = useState("")
+  const [languagesList, setLanguagesList] = useState([])
+  const [translateState, setTranslateState] = useState("");
+  const [selectedLanguageFromKey, setLanguageFromKey] = useState("")
+  const [selectedLanguageKey, setLanguageKey] = useState("")
   const [resultText, setResultText] = useState("");
   const [videoState, setVideoState] = useState("");
   const [loadVideo, setLoadVideo] = useState(false);
+  const [timeZoneState, setTimeZoneState] = useState("");
+  const [hourOption, setHourOption] = useState(-1);
+
+
 
   useEffect(() => {
     axios.get(`https://libretranslate.de/languages`)
-    .then((response) => {
-      setLanguagesList(response.data)
-    })
+      .then((response) => {
+        setLanguagesList(response.data)
+      })
   }, [])
   /*const getLanguageSource = () => {
     axios.post(`https://libretranslate.de/detect`, {
@@ -86,12 +113,12 @@ export const Navigation = () => {
       setLanguageFromKey(response.data[0].language)
     })
   }*/
-  const languageFromKey =  (selectedLanguage) => {
+  const languageFromKey = (selectedLanguage) => {
     setLanguageFromKey(selectedLanguage.target.value);
   }
-  const languageKey =  (selectedLanguage) => {
+  const languageKey = (selectedLanguage) => {
     setLanguageKey(selectedLanguage.target.value)
-  } 
+  }
 
   const extractVideoId = (url) => {
     const regExp = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})/;
@@ -107,16 +134,16 @@ export const Navigation = () => {
 
   const translateText = () => {
     let data = {
-      q : translateState,
+      q: translateState,
       source: selectedLanguageFromKey,
       target: selectedLanguageKey
     }
     axios.post(`https://libretranslate.de/translate`, data)
-    .then((response) => {
-      setResultText(response.data.translatedText)
-    })
-  } 
-    return (
+      .then((response) => {
+        setResultText(response.data.translatedText)
+      })
+  }
+  return (
     <>
       <motion.ul variants={variants}>
         {itemIds.map(i => (
@@ -128,143 +155,176 @@ export const Navigation = () => {
           if (clickState) {
             setClickState(false);
           }
-        }} 
-        variants={!clickState && menuState !== -1 ? menuVariants : menuClickedVariants} 
-        initial={!clickState || menuState === -1 ? {width: "0px", opacity: "0"} : {width: "300px", opacity: "1"}} 
-        animate={menuState !== -1 ? "open" : "closed"} 
+        }}
+        variants={!clickState && menuState !== -1 ? menuVariants : menuClickedVariants}
+        initial={!clickState || menuState === -1 ? { width: "0px", opacity: "0" } : { width: "300px", opacity: "1" }}
+        animate={menuState !== -1 ? "open" : "closed"}
         style={{
-          position: "absolute", 
-          backgroundColor: "rgba(0, 0, 0, 0.2)", 
-          top: "calc(100% - 500px)", left: "80px", 
-          width: "300px", 
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.2)",
+          top: "calc(100% - 500px)", left: "80px",
+          width: "300px",
           borderTopRightRadius: "20px",
           height: "500px"
         }}
       >
-        {menuState === 0 && <p style={{color :"white"}}>0</p>}
-        {menuState === 1 && <p style={{color :"white"}}>1</p>}
-        {menuState === 2 && <p style={{color :"white"}}>2</p>}
+        {menuState === 0 &&
+          <>
+            {hourOption !== -1   
+            ? <Moment style={{ color: "white" }} interval={1000} tz={timeZoneState} format='ddd hh:mm:ss A'>{new Date().setHours(hourOption, 0, 0)}</Moment>
+            : <Moment style={{ color: "white" }} interval={1000} tz={timeZoneState} format='ddd hh:mm:ss A'/>}
+            <select className="form-select" style={{ marginLeft: "5%", width: "40%", marginRight: "10%" }} onChange={e => setTimeZoneState(e.target.value)}>
+              <option value={moment.tz.guess(true)}>Choose a timezone</option>
+              {countryZones.map(timezone => (
+                <option value={timezone.name}>
+                  {" (" + (moment.tz(timezone.name).zoneAbbr().charAt(0) === "+" || moment.tz(timezone.name).zoneAbbr().charAt(0) === "-" ? "GMT" : moment.tz(timezone.name).zoneAbbr()) + moment.tz(timezone.name).format("Z") + ") " + timezone.name.replace(/\//g, ', ').replace(/_/g, ' ')}
+                </option>
+              )
+              )}
+            </select>
+            <select className="form-select" style={{ marginLeft: "5%", width: "40%", marginRight: "10%" }} onChange={e => setHourOption(parseInt(e.target.value))}>
+              <option value={-1}>Current time</option>
+              <option value={0}>
+                12:00 AM
+              </option>
+              {hours.map((hour) => (
+                <option value={hour}>
+                  {hour}:00 AM
+                </option>
+              ))}
+              <option value={12}>
+                12:00 PM
+              </option>
+              {hours.map((hour) => (
+                <option value={hour + 12}>
+                  {hour}:00 PM
+                </option>
+              ))}
+            </select>
+          </>}
+        {menuState === 1 && <p style={{ color: "white" }}>1</p>}
+        {menuState === 2 && <p style={{ color: "white" }}>2</p>}
         {menuState === 3 && loadVideo === false ? <>{setLoadVideo(true)}</> : <></>}
         {loadVideo &&
-        <>
-          {menuState === 3 && <p className="mt-5" style={{fontSize: "30px", color: "white"}}>YouTube Player</p>}
-          <div style={{display: "flex", justifyContent: "center"}}>
-            <div style={{display: (menuState !== 3 ? "none" : "block")}}>
-              <div style={{marginLeft: "5%", width: "90%", borderRadius: "20px", overflow: "hidden", padding: 0, height: "169px"}}>
-                <YouTube videoId={videoState !== "" ? videoState : "jfKfPfyJRdk"} 
-                opts={{
-                  height: '169',
-                  width: '100%',
-                  playerVars: {
-                    autoplay: 1,
-                  }
-                }}/>
+          <>
+            {menuState === 3 && <p className="mt-5" style={{ fontSize: "30px", color: "white" }}>YouTube Player</p>}
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <div style={{ display: (menuState !== 3 ? "none" : "block") }}>
+                <div style={{ marginLeft: "5%", width: "90%", borderRadius: "20px", overflow: "hidden", padding: 0, height: "169px" }}>
+                  <YouTube videoId={videoState !== "" ? videoState : "jfKfPfyJRdk"}
+                    opts={{
+                      height: '169',
+                      width: '100%',
+                      playerVars: {
+                        autoplay: 1,
+                      }
+                    }} />
+                </div>
+                <input
+                  type="text"
+                  className="mt-5"
+                  placeholder="Enter a youtube link"
+                  onChange={handleYoutubeInputChange}
+                  style={{
+                    fontSize: "15px",
+                    marginLeft: "0%",
+                    width: "70%",
+                    backgroundColor: "rgba(0,0,0,0)",
+                    color: "white",
+                    outline: "none",
+                    border: "none",
+                    borderBottom: "2px solid rgba(255, 255, 255, 1)",
+                  }} />
               </div>
-              <input 
-                type="text" 
-                className="mt-5"
-                placeholder="Enter a youtube link"
-                onChange={handleYoutubeInputChange}
-                style={{
-                  fontSize: "15px",
-                  marginLeft: "0%",
-                  width: "70%",
-                  backgroundColor: "rgba(0,0,0,0)",
-                  color: "white",
-                  outline: "none",
-                  border: "none",
-                  borderBottom: "2px solid rgba(255, 255, 255, 1)",
-                }}/>
             </div>
-          </div>
-        </>}
+          </>}
         {menuState === 4 &&
-        <>
-        <p className="mt-5" style={{fontSize: "30px", color: "white"}}>Translate</p>
-        <div style={{display: "flex", marginBottom: "30px"}}>
-        <select className="form-select" style={{marginLeft: "5%", width: "40%", marginRight: "10%"}} onChange={languageFromKey}>
-          <option value={""}>From</option>
-          {languagesList.map((language) => {
-            return (
-              <option value={language.code}>
-                {language.name}
-              </option>
-            )
-          })}
-        </select>
-        <select className="form-select" style={{width: "45%", marginRight: "5%"}} onChange={languageKey}>
-          <option value={""}>To</option>
-          {languagesList.map((language) => {
-            return (
-              <option value={language.code}>
-                {language.name}
-              </option>
-            )
-          })}
-        </select>
-        </div>
-        <input
-          type="text"
-          placeholder='Input text'
-          style={{
-            fontSize: "15px",
-            marginLeft: "0%",
-            width: "70%",
-            backgroundColor: "rgba(0,0,0,0)",
-            color: "white",
-            outline: "none",
-            border: "none",
-            borderBottom: "2px solid rgba(255, 255, 255, 1)",
-          }}
-          onChange={(e) => {
-            setTranslateState(e.target.value.replace("?", ""));
-          }}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              if (selectedLanguageFromKey !== "" && selectedLanguageKey !== "" && translateState !== "") {
-                translateText();
-              }
-            }
-          }}
-        />
-        {(selectedLanguageFromKey !== "" && selectedLanguageKey !== "" && translateState !== "") && 
-        <IconButton
-          size="large"
-          style={{
-            background: "none",
-            border: "none",
-            width: "30px",
-            marginLeft: "5%",
-            height: "30px"
-          }}
-          color="info"
-          onClick={translateText}
-        >
-          <TranslateIcon/></IconButton>}
-        {(selectedLanguageFromKey === "" || selectedLanguageKey === "" || translateState === "") && 
-        <IconButton
-          disabled
-          size="large"
-          style={{
-            background: "none",
-            border: "none",
-            width: "30px",
-            marginLeft: "5%",
-            height: "30px"
-          }}
-          color="info"
-          onClick={translateText}
-        >
-          <TranslateIcon/></IconButton>}
-        <br/><br/><br/>
-        <p style={{fontSize: "20px", color: "white", borderTop: "1px solid rgba(255, 255, 255, 0.3)", paddingTop: "20px"}}>Output</p>
-        <div className="translated-text" style={{overflowY: "auto", maxHeight: "170px"}}>
-          <span style={{color: "white", wordWrap: "break-word"}}>{resultText}</span>
-        </div>
-        </>
+          <>
+            <p className="mt-5" style={{ fontSize: "30px", color: "white" }}>Translate</p>
+            <div style={{ display: "flex", marginBottom: "30px" }}>
+              <select className="form-select" style={{ marginLeft: "5%", width: "40%", marginRight: "10%" }} onChange={languageFromKey}>
+                <option value={""}>From</option>
+                {languagesList.map((language) => {
+                  return (
+                    <option value={language.code}>
+                      {language.name}
+                    </option>
+                  )
+                })}
+              </select>
+              <select className="form-select" style={{ width: "45%", marginRight: "5%" }} onChange={languageKey}>
+                <option value={""}>To</option>
+                {languagesList.map((language) => {
+                  return (
+                    <option value={language.code}>
+                      {language.name}
+                    </option>
+                  )
+                })}
+              </select>
+            </div>
+            <input
+              type="text"
+              placeholder='Input text'
+              style={{
+                fontSize: "15px",
+                marginLeft: "0%",
+                width: "70%",
+                backgroundColor: "rgba(0,0,0,0)",
+                color: "white",
+                outline: "none",
+                border: "none",
+                borderBottom: "2px solid rgba(255, 255, 255, 1)",
+              }}
+              onChange={(e) => {
+                setTranslateState(e.target.value.replace("?", ""));
+              }}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  if (selectedLanguageFromKey !== "" && selectedLanguageKey !== "" && translateState !== "") {
+                    translateText();
+                  }
+                }
+              }}
+            />
+            {(selectedLanguageFromKey !== "" && selectedLanguageKey !== "" && translateState !== "") &&
+              <IconButton
+                size="large"
+                style={{
+                  background: "none",
+                  border: "none",
+                  width: "30px",
+                  marginLeft: "5%",
+                  height: "30px"
+                }}
+                color="info"
+                onClick={translateText}
+              >
+                <TranslateIcon /></IconButton>}
+            {(selectedLanguageFromKey === "" || selectedLanguageKey === "" || translateState === "") &&
+              <IconButton
+                disabled
+                size="large"
+                style={{
+                  background: "none",
+                  border: "none",
+                  width: "30px",
+                  marginLeft: "5%",
+                  height: "30px"
+                }}
+                color="info"
+                onClick={translateText}
+              >
+                <TranslateIcon /></IconButton>}
+            <br /><br /><br />
+            <p style={{ fontSize: "20px", color: "white", borderTop: "1px solid rgba(255, 255, 255, 0.3)", paddingTop: "20px" }}>Output</p>
+            <div className="translated-text" style={{ overflowY: "auto", maxHeight: "170px" }}>
+              <span style={{ color: "white", wordWrap: "break-word" }}>{resultText}</span>
+            </div>
+          </>
         }
-        {menuState === 5 && <p style={{color :"white"}}>5</p>}
-        {menuState === 6 && <p style={{color :"white"}}>6</p>}
+        {menuState === 5 && <p style={{ color: "white" }}>5</p>}
+        {menuState === 6 && <p style={{ color: "white" }}>6</p>}
         {menuState === 1}
       </motion.div>
     </>
@@ -272,3 +332,4 @@ export const Navigation = () => {
 };
 
 const itemIds = [0, 1, 2, 3, 4, 5, 6];
+const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
