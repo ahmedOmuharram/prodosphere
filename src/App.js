5ff568e218c9276a120454ec4fbd2f66e791c8f9
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-08-03 18:01:12 +0300

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import { useTimer } from 'react-timer-hook';
 import LinkGroupComponent from './LinkGroup';
 import Moment from 'react-moment';
 import CalendarComponent from './Calendar';
-import { duration } from 'moment';
 
 const unsplash = createApi({
   accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY
@@ -119,6 +118,7 @@ const CollapsingToDoList = () => {
                 onMouseLeave={() => setIsHovered(false)}
               >
                 <iframe
+                  title="World Map"
                   width="300px"
                   height="180px"
                   frameborder="0"
--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Calendar from 'react-calendar';
 import './Calendar.css';
 import Moment from 'react-moment';
